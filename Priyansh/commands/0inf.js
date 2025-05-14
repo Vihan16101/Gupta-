ae67d51e5f3d538459912979095fc0505f15489e
Rajvy71771 <rajthakur45802@gmail.com>
2025-05-14 18:38:25 +0530

    Update 0inf.js

--- Priyansh/commands/0inf.js
+++ Priyansh/commands/0inf.js
@@ -24,13 +24,13 @@ const time = process.uptime(),
 const moment = require("moment-timezone");
 var juswa = moment.tz("Asia/Kolkata").format("『D/MM/YYYY』 【HH:mm:ss】");
 var link =                                     
-["https://i.imgur.com/IZIHchw.jpeg", "https://i.imgur.com/8vJzOpH.jpeg", "https://i.imgur.com/gXj9qBA.jpeg", "https://i.imgur.com/gXj9qBA.jpeg","https://i.imgur.com/T5CMEGs.jpeg"];
+["https://i.imgur.com/C4EdQwl.jpeg", "https://i.imgur.com/VFozXtj.jpeg", "https://i.imgur.com/gXj9qBA.jpeg", "https://i.imgur.com/gXj9qBA.jpeg","https://i.imgur.com/T5CMEGs.jpeg"];
 var callback = () => api.sendMessage({body:`🌹𝙰𝙳𝙼𝙸𝙽 𝙰𝙽𝙳 𝙱𝙾𝚃 𝙸𝙽𝙵𝙾𝚁𝙼𝙰𝚃𝙸𝙾𝙽 🇮🇳 
 
 
 ☄️𝗕𝗢𝗧 𝗡𝗔𝗠𝗘☄️ ⚔ ${global.config.BOTNAME} ⚔
 
-🔥𝗢𝗪𝗡𝗘𝗥 🔥☞︎︎︎ 𝗥𝕦🅳︎🆁︎🅰︎ & ℝ𝕒𝕛 𝕏𝕕  ☜︎︎︎✰ \n\n
+🔥𝗢𝗪𝗡𝗘𝗥 🔥☞︎︎︎ & ℝ𝕒𝕛 𝕏𝕕  ☜︎︎︎✰ \n\n
 🙈🄾🅆🄽🄴🅁 🄲🄾🄽🅃🄰🄲🅃 🄻🄸🄽🄺🅂🙈➪ \n\n  𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞 🧨https://www.facebook.com/share/1EqeXH6WbN/ 💞🕊️
   \n 
 ✅𝗜𝗡𝗦𝗧𝗔𝗚𝗥𝗔𝗠 𝗨𝗦𝗘𝗥𝗡𝗔𝗠𝗘👉 @haryana_aala_sayzs \n\n  ====𝗧𝗼 𝗹𝗲𝗮𝗿𝗻 𝗛𝗼𝘄 𝘁𝗼 𝗖𝗿𝗲𝗮𝘁𝗲 𝗔 𝗯𝗼𝘁 === 𝗩𝗶𝘀𝗶𝘁 𝗔𝗻𝗱 𝗦𝘂𝗯𝘀𝗰𝗿𝗶𝗯𝗲 𝗧𝗼 𝗠𝘆 𝗖𝗵𝗮𝗻𝗻𝗲𝗹✅ 🗡 https://www.youtube.com/@mirrykal
